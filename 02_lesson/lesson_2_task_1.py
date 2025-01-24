885ce051a537cd8a77f32f95a2acb1e79ef0453e
Testing4sun <testing4sun@gmail.com>
2025-01-24 21:46:55 +0500

    02_lesson

---
+++ 02_lesson/lesson_2_task_1.py
@@ -0,0 +1,13 @@
+# Работа со списками
+
+lst = ['grape', 'berry', 'pear', 'orange', 'banana', 'apple']
+len(lst)
+
+grape = lst[0]
+berry = lst[1]
+pear = lst[2]
+orange = lst[3]
+banana = lst[4]
+apple = lst[5]
+
+print(grape, apple)
---
+++ 02_lesson/lesson_2_task_2.py
@@ -0,0 +1,10 @@
+# Високосный год
+
+def is_year_leap(year):
+    if (year % 4 == 0):
+        print("Год:", year, "True")
+    else:
+        print("Год:", year, "False")
+
+
+is_year_leap(int(input('Введите год: ')))
