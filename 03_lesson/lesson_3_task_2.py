baf15ca81b5e3d76112248ec01025cffdaca1012
Testing4sun <testing4sun@gmail.com>
2025-01-25 00:21:53 +0500

    03 lesson

---
+++ 03_lesson/lesson_3_task_2.py
@@ -0,0 +1,11 @@
+# Список объектов
+
+from smartphone import Smartphone
+
+catalog = []
+
+phone_1 = Smartphone("Samsung", "Galaxy", "+79000000000")
+phone_2 = Smartphone("Realme", "5G", "+79000010001")
+phone_3 = Smartphone("Apple", "Synergy", "+79000200002")
+phone_4 = Smartphone("Nokia", "Sun", "+79003000003")
+phone_5 = Smartphone("Lynex", "Starss", "+79000040004")
---
+++ 03_lesson/smartphone.py
@@ -0,0 +1,11 @@
+class Smartphone:
+    
+    brand = 'Samsung'
+    model = 'Galaxy'
+    number = '+79...'
+
+    def __init__ (self, brand, model, number):
+        self.brand = brand
+        self.model = model
+        self.number = number
+        print("Телефон: ", self.brand, "-", self.model, ".", self.number)
